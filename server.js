7d0381f39d1751d8202600907c8b628dcbee52e0
Ankit Saxena <ankit.saxena@deeplogictech@gmail.com>
2021-03-05 16:15:04 +0530

    Refractor Middleware

--- server.js
+++ server.js
@@ -1,29 +1,17 @@
 var express = require('express');
 var app = express();
-const PORT = 3000;
+var PORT = 3000;
 
-var middleware = {
-    requireAuthentication : function (req , res, next) {
-        console.log('private route hit !');
-        next();
-    },
-    logger : function(req,res,next) {
-        console.log('Request : '+req.method+' | '+req.originalUrl+' | '+ new Date().toString());
-        next();
-    }
-};
+var middleware = require('./middleware.js');
 
 app.use(middleware.logger);
-// app.use(middleware.requireAuthentication);
 
-app.get('/about',function(req,res){
-    res.send('About Us');
-    
-})
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+	res.send('About Us!');
+});
 
-// console.log(__dirname);
 app.use(express.static(__dirname + '/public'));
 
-app.listen(PORT,function(){
-    console.log('Express Server Started at '+PORT+' !!');
+app.listen(PORT, function () {
+	console.log('Express server started on port ' + PORT + '!');
 });

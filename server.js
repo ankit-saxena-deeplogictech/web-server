60332c61e5b4f0013fb7417e94c8da00f40a63a4
Ankit Saxena <80101818+ankit-saxena-deeplogictech@users.noreply.github.com>
2023-07-04 16:51:44 +0530

    commit 1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
-var middleware = require('./middleware.js'); // getting middleware file here 
+var middleware = require('./middleware.js'); // getting middleware file here now
 
 // this is new change
 // this is new change2
